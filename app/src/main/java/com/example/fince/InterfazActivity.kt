8bb8209989e0cbacfd2a305e8791963b79ffb2ac
martochab <martulonco@hotmail.com>
2023-10-16 20:28:48 -0300

    Subido proyecto nuevo con correcciones

---
+++ app/src/main/java/com/example/fince/InterfazActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fince
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InterfazActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interfaz)
+    }
+}
---
+++ app/src/main/java/com/example/fince/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.fince
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        supportFragmentManager.beginTransaction().add(R.id.container,LoginFragment()).commit();
+    }
+}
