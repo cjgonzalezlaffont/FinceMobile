2450f5159ebde4e6f5cdb6a65bad76671657ecdf
Guibert <34744071@comunidadort.edu.ar>
2023-11-16 18:35:57 -0300

    Se agrega logica a onViewItemDetail para que vaya al fragmento de simboloFragment. Se agrego una fun que transforma el stockModel a ActivoModel.

--- app/src/main/java/com/example/fince/AppFince.kt
+++ app/src/main/java/com/example/fince/AppFince.kt
@@ -12,7 +12,7 @@ class AppFince: Application() {
         //IPCONFIG /ALL CAMBIAR IP PARA CADA MAQUINA IPV4
         //Config.baseUrl = "http://192.168.0.8:8080/" //NAHUEL
         //Config.baseUrl="http://192.168.0.24:8080" //Martin
-        //Config.baseUrl="http://192.168.1.4:8080" //Fede
-        Config.baseUrl="http://192.168.0.14:8080" //Javier
+        Config.baseUrl="http://192.168.1.4:8080" //Fede
+        //Config.baseUrl="http://192.168.0.14:8080" //Javier
     }
 }
